edf38cd350f84d2becc6223dbee5a71de5de198a
pangnelson <87406261+pangnelson@users.noreply.github.com>
2023-04-11 10:39:48 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "database-1.ccshvphj0tcv.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "nelsonpcf-employee"
+customregion = "us-east-1"
